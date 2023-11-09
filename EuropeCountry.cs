26077697856b083aaf19215bbd583db58cae1fc5
opilane <opilane@HP-CZC2349HTB>
2023-11-09 10:00:34 +0200

    1.2

---
+++ EuropeCountry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoMapGameLabor3
+{
+    public partial class EuropeCountry : Form
+    {
+        Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
+        PictureBox pb;
+        Label l1, l2, l3;
+        public EuropeCountry()
+        {
+            this.Height = 800;
+            this.Width = 1000;
+            this.Text = "Euroopi riigid nimega";
+
+
+        }
+    }
+}
--- StartMenu.cs
+++ StartMenu.cs
@@ -1,5 +1,5 @@
 using System.Windows.Forms;
-
+//https://stackoverflow.com/questions/1733912/how-do-i-handle-dragging-of-a-label-in-c
 namespace GeoMapGameLabor3
 {
     public partial class StartMenu : Form
@@ -7,74 +7,81 @@ namespace GeoMapGameLabor3
         Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
         Rectangle Circle = new Rectangle(220, 10, 150, 150);
         Rectangle Square = new Rectangle(380, 10, 150, 150);
-        int Y_sqr = 0, X_sqr = 0, Y_crl = 0, X_crl = 0, Y_rct = 0, X_rct = 0;
+        int Y_sqr = 0, X_sqr = 0, Y_crl = 0, X_crl = 0, Y_rct = 0, X_rct = 0, X_label=0, Y_label=0;
         PictureBox pb;
         Label l1, l2, l3;
-        bool SquareBool = false, CircleBool = false, RectangleBool = false;
+        bool SquareBool = false, CircleBool = false, RectangleBool = false, LabelBool = false;
         public StartMenu()
         {
             this.Height = 800;
             this.Width = 1000;
             this.Text = "Start menu";
 
-            l1 = new Label();
-            l1.Font = new Font("Arial", 24, FontStyle.Bold);
-            l1.Text = "Vid";
-            l1.Location = new Point(10, 700);
-            l1.Size = new Size(50, 50);
-            l1.BackColor = Color.LightGray;
-            l1.BorderStyle = BorderStyle.Fixed3D;
-            this.Controls.Add(l1);
-
-            l2 = new Label();
-            l2.Font = new Font("Arial", 24, FontStyle.Bold);
-            l2.Text = "Forma";
-            l2.Location = new Point(l1.Width + 30, 700);
-            l2.Size = new Size(50, 50);
-            l2.BackColor = Color.LightGray;
-            l2.BorderStyle = BorderStyle.Fixed3D;
-            this.Controls.Add(l2);
-
-            l3 = new Label();
-            l3.Font = new Font("Arial", 24, FontStyle.Bold);
-            l3.Text = "Info";
-            l3.Location = new Point(l2.Width + 30, 700);
-            l3.Size = new Size(50, 50);
-            l3.BackColor = Color.LightGray;
-            l3.BorderStyle = BorderStyle.Fixed3D;
-            this.Controls.Add(l3);
-
             pb = new PictureBox();
 
+            
+            
+            pb.Location = new Point(10, 10);
+            
+            pb.Size = new Size(900, 700);
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.BorderStyle = BorderStyle.Fixed3D;
             pb.Paint += new PaintEventHandler(pb_Paint);
             pb.MouseDown += new MouseEventHandler(pb_MouseDown);
             pb.MouseMove += new MouseEventHandler(pb_MouseMove);
             pb.MouseUp += new MouseEventHandler(pb_MouseUp);
-            
-            pb.Location = new Point(10, 10);
-            
-            pb.Size = new Size(800, 400);
-            pb.SizeMode = PictureBoxSizeMode.Zoom;
-            pb.BorderStyle = BorderStyle.Fixed3D;
             this.Controls.Add(pb);
+
+            l1 = new Label();
+            l1.Font = new Font("Arial", 24, FontStyle.Bold);
+            l1.Text = "Vid";
+            l1.Location = new Point(10, 500);
+            l1.Size = new Size(100, 100);
+            l1.BackColor = Color.LightGray;
+            l1.BorderStyle = BorderStyle.Fixed3D;
+            pb.Controls.Add(l1);
+
+            l2 = new Label();
+            l2.Font = new Font("Arial", 24, FontStyle.Bold);
+            l2.Text = "Forma";
+            l2.Location = new Point(l1.Width + 30, 500);
+            l2.Size = new Size(100, 100);
+            l2.BackColor = Color.LightGray;
+            l2.BorderStyle = BorderStyle.Fixed3D;
+            pb.Controls.Add(l2);
+
+            l3 = new Label();
+            l3.Font = new Font("Arial", 24, FontStyle.Bold);
+            l3.Text = "Info";
+            l3.Location = new Point(l2.Width + 200, 500);
+            l3.Size = new Size(100, 100);
+            l3.BackColor = Color.LightGray;
+            l3.BorderStyle = BorderStyle.Fixed3D;
+            pb.Controls.Add(l3);
         }
 
         private void pb_MouseMove(object? sender, MouseEventArgs e)
         {
+            
             if (SquareBool)
             {
-                Square.X = e.X - Square.X;
-                Square.Y = e.Y - Square.Y;
+                Square.X = e.X - X_sqr;
+                Square.Y = e.Y - Y_sqr;
             }
             if (CircleBool)
             {
-                Circle.X = e.X - Circle.X;
-                Circle.Y = e.Y - Circle.Y;
+                Circle.X = e.X - X_crl;
+                Circle.Y = e.Y - Y_crl;
             }
             if (RectangleBool)
             {
-                Rectangle.X = e.X - Rectangle.X;
-                Rectangle.Y = e.Y - Rectangle.Y;
+                Rectangle.X = e.X - X_rct;
+                Rectangle.Y = e.Y - Y_rct;
+            }
+            if (LabelBool)
+            {
+                l2.X = e.X - X_label;
+                l2.Y = e.Y - Y_label;
             }
             pb.Invalidate();
         }
@@ -84,6 +91,14 @@ namespace GeoMapGameLabor3
             CircleBool = false;
             RectangleBool = false;
             SquareBool = false;
+
+            if((l1.Location.X < Square.X + Square.Width) && (l1.Location.X > Square.X))
+            {
+                if((l1.Location.Y < Square.Y + Square.Height) &&(l1.Location.Y > Square.Y))
+                {
+                    l3.Text = "Sinine square";
+                }
+            }
         }
 
         private void pb_MouseDown(object? sender, MouseEventArgs e)
